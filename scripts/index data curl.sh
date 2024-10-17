708b373231a3a42719d5433fc25b06138607cd62
rajavel.as <rajavel.as@flipkart.com>
2024-10-17 18:06:24 +0530

    new curls

--- scripts/index data curl.sh
+++ scripts/index data curl.sh
@@ -1,17 +1,19 @@
-curl --compressed --location 'https://www.niftyindices.com/Backpage.aspx/getTotalReturnIndexString' \
---header 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0' \
+curl --location 'https://www.niftyindices.com/Backpage.aspx/getTotalReturnIndexString' \
 --header 'Accept: application/json, text/javascript, */*; q=0.01' \
---header 'Accept-Language: en-US,en;q=0.5' \
---header 'Accept-Encoding: gzip, deflate, br' \
---header 'Content-Type: application/json; charset=utf-8' \
---header 'X-Requested-With: XMLHttpRequest' \
---header 'Origin: https://www.niftyindices.com' \
+--header 'Accept-Language: en-GB,en-US;q=0.9,en;q=0.8' \
 --header 'Connection: keep-alive' \
+--header 'Content-Type: application/json; charset=UTF-8' \
+--header 'Cookie: ak_bmsc=55AD5CF3901C56C2BE238402CB31F93C~000000000000000000000000000000~YAAQJmfRF6kHoG+SAQAAaCNmmhn+yzL/mjvE74Bva6f8DRkzdN7vqp2ZvmjzS6j+iLpVwzZvS0ayn6N+2/onbkfAKd7G5DMT/3rb5Xy78KEpIwz2OoSfhFllO8E+eXNKZuzo14AHYLEWyOUAXBQU2M7wKPBJFWnVMkcjlE+T6/vked2LJ9DeAclhIo0TDe8WUF7BfJHYXBvDdeLfqCMw0oeoIzNBbwSNVoqml3tDFYs1la1IBgaNAo0c6HxLcksZDOX6GdzKdLwl3tqfJfVwnWKg+F23gK4GxkPKX4vTGx3yCo03SvmwUo3Zwmzpr1em+BtpslNA3qEB5WznO9C9Cbd077NChHKpNas6C/NRCsQ/tgNqMwNFRmr/KQ9apRYRHERy; ASP.NET_SessionId=kh3g3btsx0qyvqdvod0m1eii; bm_sv=33E25B7ADF18CCC28D4D2FC2B748B7CC~YAAQJmfRFwoLoG+SAQAA/Elmmhm/hgBoMtTl4bQRE9cPjUE7eF7ESyOl92/5uQW7isYSyPKlcly13RvNPU+Bs2Ib3gaepug9bxMoscmQJFgB7aL4R2iBlZklQc5b+KimR3txGeq0dnz4d3HOV7Qz0Fc2Ifl4nmnOl7tbIo+4molKhsPXri62VKMW+Vhhys3BwTnq7HFrggo2/3FM4udBgHWKECtiJ5LaegpxxYM2/5s9BJRHusz+zsbs/n/LtfTw+iYy3rS0~1; bm_sv=33E25B7ADF18CCC28D4D2FC2B748B7CC~YAAQJmfRF9pwoG+SAQAAJMxrmhmklu7M+8NyGzO4yihT3w+Pcl63Fgd68E5W8/zrcVMHvWaWnLJ5I8rizoQPeNDKCPxyUbgHW1S1XEo4k1g90ZRBhoSDVqSswN2G+pcTOEPVzfyCBtTWvdCP+7zrcauAfmDxxhzFELqTMRGIDKgV1JrUun/FgiNAPHZgMhkVoTuxVmLkxxg44BNSTX3oD69IzPp3XzCTl8URhXWHqFpIWLp0/6Oqy3QiWeOvrFGhO1mUnseb~1' \
+--header 'Origin: https://www.niftyindices.com' \
 --header 'Referer: https://www.niftyindices.com/reports/historical-data' \
---header 'Cookie: ak_bmsc=45E9AF62AA9E243367D4BA7C2F405CDC~000000000000000000000000000000~YAAQFjkgF1ETJBePAQAAP99yLRfjbL2cikQLewENrrJ3hp7T7rvXJoyXTx6h0BoDMCTHWdUEQcD2igPY4nMP1rCLrps/V14lMeXnHK3dIixUKYyrQhJN6m16ciz//+y0j+lwwbpoyhNsEs9mSaWXFBsaQtMww3zkFrBLBbgmW3/HjEP1CykcrvR/stXs+eEMajwzzXpfGA+1YAv8X86F7TSl90x607eczPcqa+jnIfP8+3hwyd0o+AXZFBB01opyqQZoADREm/yx7zs4cQxEicp+U3aKm6zjys3uHSmyDoSPRmyqwIHzhlrLknGsAr2bHZ/qzyo8CDQPQDUH9Jg6GHw041MC9AhMXP3hvZ8gTG61BKPwVdnCSz8+n7EVNBXsbUe03dE=; ASP.NET_SessionId=er5k0hx30ujw13dtphywgs32; bm_sv=5CFA5373F4070A41AD097C6C084A4D69~YAAQFjkgF3ytJBePAQAAiih3LRcXOzEy3MWfL0NCEBf79mEcNl+Ed5lqhbc24tc+14mjeIUusk7E9TsDze/E8rv3sMLK+P5G6GCaZCdB2bvmNHS6PLKnqGdk7DHONfdnaOJmTojge1gEalIz1rxGMJAm3yitl0yGY5tLzD3OOa61gnFxL5piVy3sqsL9m13Ahbc96Mu0Jmk+KUkLmtmnrpAOtROt0UxVzj8Y8CF6ucgANtVAqkgn1tqm+ieBWaj619iruImbgA==~1; bm_mi=12795BBE50D0494B6093208405F12385~YAAQFjkgF/E8JBePAQAAGfJzLRde7o5X56XhggE5OwfLM3VVoyhnHcXYwYXsV89azwf0S43TgYiAgqmpvC+euH9Znt21Pob3kRZd2eavaYUhhPaAIScz7rzo6joafVRODYJ18qWvUWwvAZXyAOwZ7wWeCCjXbVt7g61HGDJ1FfX+Qgf1zYF95c9luGVNT0+aXc4mHmUYPPdrmGtkGJaa4nYFQCzpeqfVEbSIKRlqTMHDcMx0dcqraVKZJ6PbGyhpyLJAaiDL2l4MWhOErsnlwCgkkhuoBM33KSq9/Zn0830xxZbSXtRZB76L4L7NPrOHuA7U0JblSEZLAdTJWfprGTC1dki/v8rvz13Z~1; bm_sv=5CFA5373F4070A41AD097C6C084A4D69~YAAQ0YMsMU/kpueOAQAAsKB9LRflnj7DCsvSKryVuOMDT64HjiviVIOVDayCMfcbFPvw9ota3pgHzibWnOtvM3MX+WXbmYksg+g/g88FDSnZ17R59n7rN8K8/6ihMy4WPWk+yphsTwT5BgB9hOCm8q3Ujqx77Qf4HUrnLj/c8ct4GWdhW21dOQDlLBbfDTlCeVVbz5mxMOwj+aelwCKm2fZYlcBL5/P/KAvhLmQS9Lufj+PzgjT2fV0X/ajeA9vyxA22/+CYOg==~1; ak_bmsc=45E9AF62AA9E243367D4BA7C2F405CDC~000000000000000000000000000000~YAAQ0YMsMSH9pueOAQAAOQ+LLRfPM2hbWyP75a3CjqH8RlGFXRcWNz21SgRDHhN7rsjAdnHWaKWOswwKvBPLvCHzYb96deDoD1pquutJwLvXaxky09DD5kQHp8O2UOKkKwjEB1eopO8IZZX9N5h0xhq1BQtwOX3D/88yCWY5l4aIdzuDj3j2tXLJD1WHw7c/yg2DkZQRR05cvjKBxmSSh9bRlWuHP/j+pEgE3eUsr2O/Ph3dj1QCYx7YA6t8U4rjdlz9oweVOOdSg0aFCb7Os6+mZ+LDImIsXQ2zhYWw/HzAX4LpoxuPJMZovuP0NbnyJF3EI0RrXrbESJIDv8OHWOlmcXPRzQv7yyiwlphf8KZK65TJN7u9Sfpe//TPWGqN9eF+3DHqYpbUYLepHwTwJQO4; bm_sv=5CFA5373F4070A41AD097C6C084A4D69~YAAQ0YMsMSL9pueOAQAAOQ+LLRe8FMMHEbtLZZZQiecLajcw4E6HKdiDs1gtbNLezVIHx4io9f2niH+erEpYqXNFlK1RK+pi9crxM2VZk5L5urBv4W2/BKYj9Ysnu01jTIdkwPcbzzOcb6P0D+qNJ9pndOVa+EZj8w+c2jcRmlUTnEWUXDrg3xHlHFEiIVa6hzpSrL0aGaJS2rDmGLjhl0LdDVvH1xKJnAsegTnFQGtE3c/Q6tE2JynkOLGhBmYXctNTIpbfZw==~1' \
 --header 'Sec-Fetch-Dest: empty' \
 --header 'Sec-Fetch-Mode: cors' \
 --header 'Sec-Fetch-Site: same-origin' \
+--header 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36' \
+--header 'X-Requested-With: XMLHttpRequest' \
+--header 'sec-ch-ua: "Google Chrome";v="129", "Not=A?Brand";v="8", "Chromium";v="129"' \
+--header 'sec-ch-ua-mobile: ?0' \
+--header 'sec-ch-ua-platform: "macOS"' \
 --data '{
-    "cinfo": "{'\''name'\'':'\''NIFTY MIDCAP 150'\'','\''startDate'\'':'\''01-Jan-1995'\'','\''endDate'\'':'\''30-Apr-2024'\''}"
+    "cinfo": "{'\''name'\'':'\''NIFTY 50'\'','\''startDate'\'':'\''01-Oct-2024'\'','\''endDate'\'':'\''17-Oct-2024'\'','\''indexName'\'':'\''NIFTY 50'\''}"
 }'
